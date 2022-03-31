07879e1e43061678c7ee0920c7013c9d2bbd40c3
Amaneet Hundal <amanhundal19@gmail.com>
2022-03-30 17:21:03 -0700

    creation of first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import GameIndex from './components/weeklyGame/GameIndex';
 
 function App() {
   return (
     <div className="App">
       <p>Welcome to KewlKewlKewl's website!</p>
+      <GameIndex />
     </div>
   );
 }
---
+++ frontend/src/components/weeklyGame/GameIndex.tsx
@@ -0,0 +1,7 @@
+const GameIndex = (props: {}) => {
+    return (
+        <h1>Table of TV SHOW</h1>
+    )
+};
+
+export default GameIndex;
