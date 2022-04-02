625936e710905bbda87273325aafe7d71b3f4e6d
Amaneet Hundal <amanhundal19@gmail.com>
2022-04-01 23:48:13 -0700

    95% working state for user guess

--- frontend/src/components/weeklyGame/UserGuess.tsx
+++ frontend/src/components/weeklyGame/UserGuess.tsx
@@ -2,22 +2,29 @@ import "../styles/UserGuess.css";
 import {useState} from 'react';
 
 const UserGuess = (props: any) => {
-  const [guess, setGuess] = useState("");
   const {answer} = props;
   const answerArray = answer.split("");
+  const [guess, setGuess]: any = useState({});
 
-  const submitAnswer = (input: string) => {
+  const submitAnswer = (input: any) => {
     if(input === "Enter") {
-      console.log(guess.toUpperCase());
+      console.log(guess)
     }
   }
 
   return (
     <form onKeyDown={event => {submitAnswer(event.key)}} autoComplete="off">
       <div className="user-input">
-        {answerArray.map((char: string, index: number) => {
+        {answerArray.map((char: any, index: number) => {
           if(char !== " ") {
-            return <input type="text" key={index} onChange={event => setGuess((prev) => prev + event.target.value)}  maxLength={1} className="input-boxes"></input>
+            const objKey = index.toString();
+            return <input type="text" key={index} onChange={(event) => {
+              const newKey = guess[index] = event.target.value;
+              setGuess((prev: any) => ({...prev, newKey}));
+              // console.log(objKey);
+              // console.log(event.target.value);
+              // console.log(guess)
+            }}  maxLength={1} className="input-boxes"></input>
           }
         })}
       </div>
