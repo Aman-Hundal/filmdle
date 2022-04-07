4029ce4719b5ce92898d2f171e2d4068ab9fd54d
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2022-04-07 13:41:32 -0700

    small app clean up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,17 +10,6 @@ function App() {
     gameOverCheck
   } = useAppData(); 
 
-  // const mockMovieData: {name: string, image: string, releaseDate: string, runTime: string, rating: string, contentRating: string, genres: string, accolades: string} = {
-  //   name: "Goodfellas",
-  //   image: "https://static.independent.co.uk/2020/09/17/10/goodfellas-poster.jpg?quality=75&width=990&auto=webp&crop=982:726,smart",
-  //   releaseDate: "1990",
-  //   runTime: "2hr and 20mins",
-  //   rating: "9.0",
-  //   contentRating: "R",
-  //   genres: "Thriller, Drama",
-  //   accolades: "5 nominations, no wins"
-  // };
-
   console.log(state.movie)
   console.log(state.answer)
   
