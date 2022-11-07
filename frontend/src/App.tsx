39e055863df2212934d47b68b4ce70f9ea772230
Aman-Hundal <amanhundal19@gmail.com>
2022-11-07 11:39:44 -0800

    added a nav bar and adjusted movie details settings. WIP final styling for desktop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import GameIndex from './components/Game/Index';
 import useAppData from './hooks/useAppData';
+import NavBar from './components/Navigation/NavBar';
 
 function App() {
   const {
@@ -20,8 +21,8 @@ function App() {
     <>
       {!loading ?
         <div className="App">
-          <h1 className="title">filmdle</h1>
-          {gameState.gameOver || gameState.isCorrect ? <p className="goodbye">The movie was <strong>{movieState.title.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")}</strong>. Please join again next week.</p> : null}
+          <NavBar />
+          {gameState.gameOver || gameState.isCorrect ? <p className="goodbye">The movie was <strong>{movieState.title.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")}</strong>. Please join again next week.</p> : <p className="goodbye"></p>}
           <GameIndex
             focusField={focusField}
             isCorrect={gameState.isCorrect}
---
+++ frontend/src/components/Navigation/NavBar.tsx
@@ -0,0 +1,33 @@
+import {
+    Grid,
+    Stack,
+} from '@mui/material';
+import { grey } from '@mui/material/colors';
+
+const NavBar = (prop: any) => {
+
+    return (
+        <Grid
+            container
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+                width: "100%",
+                padding: "0.5%",
+                backgroundColor: grey[900]
+            }}
+        >
+            <Grid item>
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <h1 style={{ color: "#ffffff" }} className="title">filmdle</h1>
+                </Stack>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default NavBar;
