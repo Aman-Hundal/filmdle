118bfe7b6db21b29dbfc054e24e4210ce69c0c69
Aman-Hundal <amanhundal19@gmail.com>
2022-11-07 12:43:21 -0800

    Commit before movie info restyle

--- frontend/src/components/Game/MovieDetails/MovieInfo.tsx
+++ frontend/src/components/Game/MovieDetails/MovieInfo.tsx
@@ -1,3 +1,5 @@
+
+import { Stack } from "@mui/material";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import MovieIcon from '@mui/icons-material/Movie';
 import TheatersIcon from '@mui/icons-material/Theaters';
@@ -14,25 +16,29 @@ const MovieInfo = (props: any) => {
     const answerLetterCount: number = answer.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s+/g, "").toLowerCase().length;
 
     return (
-        <ul className="details-list">
-            <br />
-            {movieData.year ? <li>Release Date: {movieData.year}</li> : <li>Release Date: N/A</li>}
-            <br />
-            {movieData.runtimeStr ? <li>Runtime: {movieData.runtimeStr}</li> : <li>Runtime: N/A</li>}
-            <br />
-            {movieData.metacriticRating ? <li>Metacritic Rating: {movieData.metacriticRating}/100</li> : <li>Metacritic Rating: N/A</li>}
-            <br />
-            {movieData.imDbRating ? <li>IMDB Rating: {movieData.imDbRating}/10</li> : <li>IMDB Rating: N/A</li>}
-            <br />
-            {movieData.contentRating ? <li>Content Rating: {movieData.contentRating}</li> : <li>Content Rating: N/A</li>}
-            <br />
-            {movieData.genres ? <li>Genres: {movieData.genres}</li> : <li>Genres: N/A</li>}
-            <br />
-            {movieData.awards ? <li>Accolades: IMDB {movieData.awards}</li> : <li>Accolades: N/A</li>}
-            <br />
-            <li>Letters: {answerLetterCount}</li>
-            <br />
-        </ul>
+        <Stack
+            sx={{ width: "100%", height: "100%", }}
+        >
+            <ul style={{ listStyle: "none", textAlign: "left" }}>
+                <br />
+                {movieData.year ? <li>Release Date: {movieData.year}</li> : <li>Release Date: N/A</li>}
+                <br />
+                {movieData.runtimeStr ? <li>Runtime: {movieData.runtimeStr}</li> : <li>Runtime: N/A</li>}
+                <br />
+                {movieData.metacriticRating ? <li>Metacritic Rating: {movieData.metacriticRating}/100</li> : <li>Metacritic Rating: N/A</li>}
+                <br />
+                {movieData.imDbRating ? <li>IMDB Rating: {movieData.imDbRating}/10</li> : <li>IMDB Rating: N/A</li>}
+                <br />
+                {movieData.contentRating ? <li>Content Rating: {movieData.contentRating}</li> : <li>Content Rating: N/A</li>}
+                <br />
+                {movieData.genres ? <li>Genres: {movieData.genres}</li> : <li>Genres: N/A</li>}
+                <br />
+                {movieData.awards ? <li>Accolades: IMDB {movieData.awards}</li> : <li>Accolades: N/A</li>}
+                <br />
+                <li>Letters: {answerLetterCount}</li>
+                <br />
+            </ul>
+        </Stack>
     );
 }
 
